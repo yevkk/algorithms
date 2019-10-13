1138740221dd2545309ac97cb199fe408f8c6517
yevkk <yevkk@users.noreply.github.com>
2019-10-14 01:29:54 +0300

    add basic methods

--- ones_and_zeros_sort/main.cpp
+++ ones_and_zeros_sort/main.cpp
@@ -1,37 +1,68 @@
 #include <iostream>
 
-template <typename T>
-class Node{
+template<typename T>
+class Node {
 public:
     int key;
-    T* data;
-  Node(int Key, T Data){
-      key = Key;
-      data = Data;
-  }
+    T *data;
+
+    Node(int Key, T Data) {
+        key = Key;
+        data = Data;
+    }
 };
 
-template <typename T>
-class DataSet{
+template<typename T>
+class DataSet {
 private:
     int capacity;
 
-    void grow_capacity() {
-        if (capacity < 100) capacity *= 4;
-        else capacity *= 2;
-
-        int *tmp = elements;
-        elements = new int[capacity];
-        for (int i = 0; i < size; i++)
-            elements[i] = tmp[i];
-        delete tmp;
-    }
+    void grow_capacity();
 
 public:
-    Node<T>* elements;
+    int size;
+    Node<T> *elements;
+
+    DataSet();
+
+    void add_element(int key, T data);
+
+    void print();
 };
 
+template<typename T>
+void DataSet<T>::grow_capacity() {
+    if (capacity < 100) capacity *= 4;
+    else capacity *= 2;
+
+    int *tmp = elements;
+    elements = new int[capacity];
+    for (int i = 0; i < size; i++)
+        elements[i] = tmp[i];
+    delete tmp;
+}
+
+template<typename T>
+DataSet<T>::DataSet() {
+    size = 0;
+    capacity = 1;
+    elements = new T[capacity];
+}
+
+template<typename T>
+void DataSet<T>::add_element(int key, T data) {
+    if (size == capacity) grow_capacity();
+    elements[size] = {key, data};
+    size++;
+}
+
+template<typename T>
+void DataSet<T>::print() {
+    for (int i = 0; i < size; i++)
+        std::cout << elements[i].data << '(' << elements[i].key << ')' << "  ";
+    std::cout << std::endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
