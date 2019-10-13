73b1eb0f093f5374ff624c507b4fcce8f3f1d650
yevkk <yevkk@users.noreply.github.com>
2019-10-14 01:39:04 +0300

    added sort1() method

--- ones_and_zeros_sort/main.cpp
+++ ones_and_zeros_sort/main.cpp
@@ -4,7 +4,9 @@ template<typename T>
 class Node {
 public:
     int key;
-    T *data;
+    T data;
+
+    Node() = default;
 
     Node(int Key, T Data) {
         key = Key;
@@ -28,6 +30,10 @@ public:
     void add_element(int key, T data);
 
     void print();
+
+    void sort1();
+
+    void sort2();
 };
 
 template<typename T>
@@ -35,8 +41,8 @@ void DataSet<T>::grow_capacity() {
     if (capacity < 100) capacity *= 4;
     else capacity *= 2;
 
-    int *tmp = elements;
-    elements = new int[capacity];
+    Node<T> *tmp = elements;
+    elements = new Node<T>[capacity];
     for (int i = 0; i < size; i++)
         elements[i] = tmp[i];
     delete tmp;
@@ -46,7 +52,7 @@ template<typename T>
 DataSet<T>::DataSet() {
     size = 0;
     capacity = 1;
-    elements = new T[capacity];
+    elements = new Node<T>[capacity];
 }
 
 template<typename T>
@@ -63,6 +69,39 @@ void DataSet<T>::print() {
     std::cout << std::endl;
 }
 
+template<typename T>
+void DataSet<T>::sort1() {
+    DataSet<T> subset0, subset1;
+    for (int i = 0; i < size; i++) {
+        if (elements[i].key == 0)
+            subset0.add_element(0, elements[i].data);
+        else
+            subset1.add_element(1, elements[i].data);
+    }
+
+    for (int i = 0; i < subset0.size; i++){
+        elements[i] = subset0.elements[i];
+    }
+
+    for (int i = 0; i < subset1.size; i++){
+        elements[subset0.size + i] = subset1.elements[i];
+    }
+}
+
 int main() {
+    DataSet<int> set;
+    set.add_element(0, 3);
+    set.add_element(1, 7);
+    set.add_element(1, 4);
+    set.add_element(0, 3);
+    set.add_element(0, 11);
+    set.add_element(1, 8);
+    set.add_element(0, 13);
+    set.add_element(1, 5);
+    set.add_element(0, 2);
+    set.print();
+    set.sort1();
+    set.print();
+
     return 0;
 }
