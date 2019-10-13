242c39eb06babb9f748e4a57b7ad33d084773238
yevkk <yevkk@users.noreply.github.com>
2019-10-14 00:35:34 +0300

    add extract_max() method

--- d-ary_heap/main.cpp
+++ d-ary_heap/main.cpp
@@ -45,12 +45,15 @@ private:
 
     void restore_down(int index) {
         int max_child_i;
+        int tmp;
         while (true) {
             if ((index * d + 1) >= size) break;
             max_child_i = -1;
+            tmp = elements[index];
             for (int i = 1; i <= d; i++) {
-                if ((elements[index] < elements[i]) && (elements[max_child_i] < elements[i])) {
-                    max_child_i = i;
+                if (tmp < elements[index * d + i]) {
+                    max_child_i = index * d + i;
+                    tmp = elements[index * d + i];
                 }
             }
             if (max_child_i != -1) {
@@ -80,10 +83,22 @@ public:
         size++;
     }
 
+    int extract_max() {
+        if (size == 0) return 0;
+        if (size - 1 < capacity / 2) shrink_capacity();
+        int res = elements[0];
+
+        elements[0] = elements[size - 1];
+        size--;
+        restore_down(0);
+
+        return res;
+    }
+
     void print(int lvl = 0, int index = 0) {
         if (index >= size) return;
         for (int i = 0; i < lvl; i++) {
-            std::cout << '\t';
+            std::cout << "|\t";
         }
         std::cout << "| " << elements[index] << std::endl;
         for (int i = 1; i <= d; i++) {
@@ -103,8 +118,33 @@ int main() {
     heap.insert(5);
     heap.insert(8);
     heap.insert(3);
+    heap.insert(21);
+    heap.insert(14);
+    heap.insert(7);
+    heap.insert(13);
+    heap.insert(2);
+    heap.insert(1);
+    heap.insert(12);
+    heap.insert(11);
+    heap.insert(9);
+    heap.insert(23);
+    heap.insert(26);
+    heap.insert(2);
 
     heap.print();
+    std::cout << std::endl;
+
+
+    heap.extract_max();
+    heap.extract_max();
+    heap.extract_max();
+    heap.extract_max();
+    heap.extract_max();
+
+
+
+    heap.print();
+
 
     return 0;
 }
