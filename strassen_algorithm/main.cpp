93b98569ca4b1d4412822280ec01db2a57a0dc16
yevkk <yevkk@users.noreply.github.com>
2019-10-20 15:37:31 +0300

    created Matrix class, added constructors and destructor

---
+++ strassen_algorithm/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <ctime>
+#include <random>
+
+int rand_int(int min, int max) {
+    static std::random_device rd;
+    static std::seed_seq seed{rd(), static_cast<unsigned int>(time(nullptr))};
+    static std::mt19937_64 gen(seed);
+    std::uniform_int_distribution<int> dist(min, max);
+
+    return dist(gen);
+}
+
+class Matrix {
+private:
+    const int _MIN_RAND_VALUE = 0;
+    const int _MAX_RAND_VALUE = 10;
+public:
+    struct {
+        int rows;
+        int columns;
+    } size = {};
+
+    int **elements;
+
+    Matrix(int *array, const int &size_r, const int &size_c) {
+        elements = new int *[size_r];
+        for (int i = 0; i < size_r; i++) {
+            elements[i] = new int[size_c];
+            std::copy(array + i * size_c, array + i * size_c + size_r + 1, elements[i]);
+        }
+        size = {size_r, size_c};
+    }
+
+    Matrix(const int &size_r, const int &size_c, const int &zero_flag = 0) {
+        elements = new int *[size_r];
+        for (int i = 0; i < size_r; i++) {
+            elements[i] = new int[size_c];
+            for (int j = 0; j < size_c; j++)
+                elements[i][j] = (zero_flag) ? 0 : rand_int(_MIN_RAND_VALUE, _MAX_RAND_VALUE);
+        }
+        size = {size_r, size_c};
+    }
+
+    ~Matrix() {
+        for (int i = 0; i < size.rows; i++)
+            delete[] elements[i];
+        delete &size;
+    }
+
+    void print() {
+        for (int i = 0; i < size.rows; i++) {
+            std::cout << i << ") ";
+            for (int j = 0; j < size.columns; j++) {
+                std::cout << elements[i][j] << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
+int main() {
+    int a[] = {3, 2, 3, 2, 6, 7, 8, 9, 1, 2, 3, 4};
+    //for (int i = 0; i < 4; i++)
+    //a[i] = new int[4];
+
+    Matrix M(a, 3, 4);
+    M.print();
+    return 0;
+}
