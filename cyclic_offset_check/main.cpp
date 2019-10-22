d0c48dfed744ce61552a720f1b48e2b6f1d007c4
yevkk <yevkk@users.noreply.github.com>
2019-10-22 07:57:31 +0300

    changed int to unsigned int where needed

--- cyclic_offset_check/main.cpp
+++ cyclic_offset_check/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 
-int *zFunction(const std::string &str) {
-    int *z = new int[str.size()];
+unsigned int *zFunction(const std::string &str) {
+    auto *z = new unsigned int[str.size()];
     z[0] = 0;
-    int left = 0;
-    int right = 0;
+    unsigned int left = 0;
+    unsigned int right = 0;
     for (unsigned int i = 1; i < str.size(); i++) {
         if (i > right) {
             left = right = i;
@@ -15,12 +15,10 @@ int *zFunction(const std::string &str) {
             z[i] = right - left;
             right--;
         } else {
-            //we are operating inside box
-            int j = i - left;
-            //if value does not stretches till right bound then just copy it.
+            unsigned int j = i - left;
             if (z[j] < right - i + 1) {
                 z[i] = z[j];
-            } else { //otherwise try to see if there are more matches.
+            } else {
                 left = i;
                 while (right < str.size() && str[right] == str[right - left]) {
                     right++;
@@ -35,7 +33,7 @@ int *zFunction(const std::string &str) {
 
 bool isCyclicOffset(const std::string &str1, const std::string &str2) {
     std::string tmp = str1 + (char) CHAR_MAX + str2;
-    int *z = zFunction("abaabbaba");
+    auto *z = zFunction("abaabbaba");
     for (unsigned int i = 0; i < 9; i++)
         std::cout << z[i] << " ";
 
