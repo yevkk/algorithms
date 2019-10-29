d744bc1a1f4e423ebcc8bf8589596a6ce87e34c2
yevkk <yevkk@users.noreply.github.com>
2019-10-29 22:47:23 +0200

    changed hash calculation

--- rabin-karp/main.cpp
+++ rabin-karp/main.cpp
@@ -1,26 +1,43 @@
-#include "rabin_karp_extended.hpp"
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "tests.hpp"
 #include <vector>
+#include "rabin_karp_extended.hpp"
 
-int main() {
-    std::vector<std::vector<char>> a = {{'a', 'f', 'g', 'd'},
-                                       {'v', 'a', 'f', 'o'},
-                                       {'g', 'v', 'a', 'a'},
-                                       {'a', 'f', 'g', 'b'}};
-
-    std::vector<std::vector<char>> b = {{'a', 'f'},
-                                       {'v', 'a'}};
-    Matrix<char> M(4);
-    Matrix<char> P(2);
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++)
-            M(i, j) = a[i][j];
-
-    for (int i = 0; i < 2; i++)
-        for (int j = 0; j < 2; j++)
-            P(i, j) = b[i][j];
-
-    std::cout << std::endl << rabin_karp_extended<char>(M, P);
-
-    return 0;
-}
+//int main() {
+////    Matrix<int> M(4), P(2);
+////
+////    std::vector<std::vector<int>> a = {{1, 5, 3, 4},
+////                                        {7, 1, 5, 6},
+////                                        {3, 7, 1, 1},
+////                                        {1, 5, 3, 2}};
+////
+////
+////    std::vector<std::vector<int>> b = {{1, 5},
+////                                        {7, 1}};
+//    Matrix<char> M(4), P(2);
+//
+//    std::vector<std::vector<char>> a = {{'a', 'f', 'g', 'd'},
+//         {'v', 'a', 'f', 'o'},
+//         {'g', 'v', 'a', 'a'},
+//         {'a', 'f', 'g', 'b'}};
+//
+//    std::vector<std::vector<char>> b = {{'a', 'f'},
+//         {'v', 'a'}};
+//
+//    for (int i = 0; i < 4; i++)
+//        for (int j = 0; j < 4; j++)
+//            M(i, j) = a[i][j];
+//
+//    for (int i = 0; i < 4; i++)
+//        for (int j = 0; j < 4; j++)
+//            M(i, j) = a[i][j];
+//
+//    for (int i = 0; i < 2; i++)
+//        for (int j = 0; j < 2; j++)
+//            P(i, j) = b[i][j];
+//
+//    std::cout << rabin_karp_extended(M, P);
+//
+//    return 0;
+//}
--- rabin-karp/rabin_karp_extended.hpp
+++ rabin-karp/rabin_karp_extended.hpp
@@ -1,14 +1,37 @@
 #pragma once
 
 #include "matrix.hpp"
+#include <cmath>
 
-int hash(char c) {
+int for_hash(char c) {
+    return c - 'a' + 1;
+}
+
+int for_hash(int c) {
     return c;
 }
 
-int hash(int c) {
-    return c;
+int mod(int num1, int num2) {
+    return ((num1 % num2) >= 0) ? (num1 % num2) : ((num1 % num2) + num2);
 }
+
+template<typename T>
+int D(){
+    return 0;
+};
+
+template<>
+int D<int>(){
+    return 10;
+};
+
+template<>
+int D<char>(){
+    return 26;
+};
+
+const int Q = 23;
+
 
 template<typename T>
 bool matrix_match(Matrix<T> M, int r_index, int c_index, Matrix<T> P) {
@@ -23,45 +46,47 @@ template<typename T>
 int rabin_karp_extended(Matrix<T> M, Matrix<T> P) {
     int res = 0;
     if (P.size > M.size) return 0;
-
-    M.print(); //TODO: delete;
-    std::cout << std::endl;
+    Matrix<int> c_hashes(M.size);
 
     int p_hash = 0;
-    for (int i = 0; i < P.size; ++i)
-        for (int j = 0; j < P.size; ++j)
-            p_hash += hash(P(i, j));
-
-    Matrix<int> c_hashes(M.size);
-    for (int i = 0; i < c_hashes.size; ++i) {
+    std::vector<int> tmp_hash;;
+    for (int i = 0; i < P.size; ++i) {
+        tmp_hash.push_back(0);
         for (int j = 0; j < P.size; ++j) {
-            c_hashes(0, i) += hash(M(j, i));
-        }
-        for (int j = 1; j < (c_hashes.size - P.size + 1); ++j) {
-            c_hashes(j, i) = c_hashes(j - 1, i) - hash(M(j - 1, i)) + hash(M(j + P.size - 1, i));
+            tmp_hash[i] = mod(tmp_hash[i] * D<T>() + for_hash(P(j, i)), Q);
         }
     }
 
-    c_hashes.print(); //TODO: delete;
-    std::cout << std::endl;
+    for (int i = 0; i < P.size; ++i) {
+        p_hash = mod(p_hash * D<T>() + for_hash(tmp_hash[i]), Q);
+    }
+
+    int h = (int) pow(D<T>(), P.size - 1) % Q;
+    for (int i = 0; i < c_hashes.size; ++i) {
+        for (int j = 0; j < P.size; ++j) {
+            c_hashes(0, i) = mod(c_hashes(0, i) * D<T>() + for_hash(M(j, i)), Q);
+        }
+        for (int j = 1; j < (c_hashes.size - P.size + 1); ++j) {
+            c_hashes(j, i) =
+                    mod(D<T>() * (c_hashes(j - 1, i) - for_hash(M(j - 1, i)) * h) + for_hash(M(j + P.size - 1, i)), Q);
+        }
+    }
 
     Matrix<int> r_hashes(M.size - P.size + 1);
     for (int i = 0; i < r_hashes.size; ++i) {
         for (int j = 0; j < P.size; ++j) {
-            r_hashes(i, 0) += hash(c_hashes(i, j));
+            r_hashes(i, 0) = mod(r_hashes(i, 0) * D<T>() + for_hash(c_hashes(i, j)), Q);
         }
         if (r_hashes(i, 0) == p_hash)
             if (matrix_match(M, i, 0, P)) res++;
         for (int j = 1; j < (c_hashes.size - P.size + 1); ++j) {
-            r_hashes(i, j) = r_hashes(i, j - 1) - hash(c_hashes(i, j - 1)) + hash(c_hashes(i, j + P.size - 1));
+            r_hashes(i, j) = mod(D<T>() * (r_hashes(i, j - 1) - for_hash(c_hashes(i, j - 1)) * h) +
+                              for_hash(c_hashes(i, j + P.size - 1)), Q);
 
             if (r_hashes(i, j) == p_hash)
                 if (matrix_match(M, i, j, P)) res++;
         }
     }
-
-    r_hashes.print(); //TODO: delete;
-    std::cout << std::endl;
 
 
     return res;
