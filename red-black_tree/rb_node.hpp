bf91d2787bcaeea4cde418a0b690e456cef171af
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-03-01 09:22:33 +0200

    implemented rb_node

---
+++ red-black_tree/rb_node.hpp
@@ -0,0 +1,61 @@
+#ifndef RED_BLACK_TREE_RB_NODE_HPP
+#define RED_BLACK_TREE_RB_NODE_HPP
+
+enum COLOR {RED = 0, BLACK};
+
+template <typename DataType>
+class RBNode {
+private:
+    COLOR _color;
+    RBNode* _parent;
+    RBNode* _left;
+    RBNode* _right;
+    DataType* _data;
+public:
+    RBNode (DataType* data, RBNode* parent, RBNode* left, RBNode* right, COLOR color = RED):
+    _data(data),
+    _parent(parent),
+    _left(left),
+    _right(right),
+    _color(color) {};
+
+    static RBNode* nullNode () {
+        static RBNode* ptr = new RBNode(nullptr, ptr, ptr, ptr, BLACK);
+        return ptr;
+    }
+
+    void switchColor() {
+        if (_color == BLACK) {
+            _color = RED;
+        } else {
+            _color = BLACK;
+        }
+    }
+
+    void setColor(COLOR color) {
+        _color = color;
+    }
+
+    COLOR color() {
+        return _color;
+    };
+
+    RBNode* parent() {
+        return _parent;
+    };
+
+    RBNode* left() {
+        return _left;
+    };
+
+    RBNode* right() {
+        return _right;
+    };
+
+    DataType* data() {
+        return _data;
+    };
+};
+
+
+#endif //RED_BLACK_TREE_RB_NODE_HPP
