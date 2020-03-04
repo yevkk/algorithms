1664eecaddac5832a02dad2bf4545d3b4a897329
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-03-05 01:09:41 +0200

    declared and implemented binomial node

--- binomial_heap/binominal_node.hpp
+++ binomial_heap/binominal_node.hpp
@@ -1,9 +1,24 @@
 #ifndef BINOMIAL_HEAP_BINOMINAL_NODE_HPP
 #define BINOMIAL_HEAP_BINOMINAL_NODE_HPP
 
+#include "department.hpp"
 
 class BinomialNode {
+private:
+    Student *_data;
+    int _degree;
+public:
+    BinomialNode *parent;
+    BinomialNode *sibling;
+    BinomialNode *child;
 
+    explicit BinomialNode(Student *data);
+
+    Student *data();
+
+    int degree();
+
+    void incDegree();
 };
 
 
