e2ce591c55d165458d6cc87d20af5000f02e7cc4
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-03-14 15:26:32 +0200

    example added

--- splay_tree/main.cpp
+++ splay_tree/main.cpp
@@ -1,6 +1,17 @@
 #include "department.hpp"
 #include "splay_tree.hpp"
 #include <iostream>
+#include <random>
+#include <ctime>
+
+int rand_int(int min, int max) {
+    static std::random_device rd;
+    static std::seed_seq seed{rd(), static_cast<unsigned int>(time(nullptr))};
+    static std::mt19937_64 gen(seed);
+    std::uniform_int_distribution<int> dist(min, max);
+
+    return dist(gen);
+}
 
 void example1(int count = 10) {
     auto students = getStudentsFromDB("../../department.db");
@@ -13,7 +24,24 @@ void example1(int count = 10) {
     }
 }
 
+void example2(int count = 10) {
+    int* num = new int(count);
+    SplayTree<int> tree;
+
+    for (int i = 0; i < count; i++) {
+        num[i] = rand_int(0, 50);
+        tree.insert(num[i]);
+        tree.print();
+        std::cout << std::endl << std::endl;
+    }
+
+    int index = 5;
+    std::cout << "Searching for " << num[index] << std::endl;
+    std::cout << "Result: " << std::boolalpha << tree.contains(num[index]) << std::endl;
+    tree.print();
+}
+
 int main() {
-    example1(10);
+    example2(11);
     return 0;
 }
