0f596360bdaad5eb0edd8002476d446b8af5fb04
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-03-17 18:33:31 +0200

    fix

--- optimal_bst/main.cpp
+++ optimal_bst/main.cpp
@@ -1,8 +1,21 @@
 #include "bst.hpp"
+#include "optimal_bst.hpp"
 
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int nums[] = {1, 2, 3, 4, 5};
+    std::vector<double> p = {0.15, 0.10, 0.05, 0.10, 0.20};
+    std::vector<double> q = {0.05, 0.10, 0.05, 0.05, 0.05, 0.10};
+
+
+    auto rootMatrix = optimalBSTRootMatrix(p, q);
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            std::cout << rootMatrix[i][j] + 1 << "   ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
--- optimal_bst/optimal_bst.hpp
+++ optimal_bst/optimal_bst.hpp
@@ -1,10 +1,13 @@
 #ifndef OPTIMAL_BST_OPTIMAL_BST_HPP
 #define OPTIMAL_BST_OPTIMAL_BST_HPP
 
+#include "bst.hpp"
+
 #include <vector>
 #include <climits>
+#include <cassert>
 
-int** optimalBSTRootMatrix(std::vector<double> p, std::vector<double> q) {
+int **optimalBSTRootMatrix(std::vector<double> &p, std::vector<double> &q) {
     int n = p.size();
 
     double **subtree_cost = new double *[n + 2];
@@ -17,27 +20,80 @@ int** optimalBSTRootMatrix(std::vector<double> p, std::vector<double> q) {
         root[i] = new int[n + 1];
     }
 
-    for (int i = 1; i <= n; i++) {
+    //TODO: clear
+    for (int i = 1; i <= n + 1; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            subtree_cost[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i <= n + 1; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            subtree_exp[i][j] = 0;
+        }
+    }
+
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 1; j < n + 1; j++) {
+            root[i][j] = -1;
+        }
+    }
+
+    //=======
+
+    for (int i = 1; i <= n + 1; i++) {
         subtree_cost[i][i - 1] = q[i - 1];
         subtree_exp[i][i - 1] = q[i - 1];
     }
 
     for (int length = 1; length <= n; length++) {
-        for (int i = 1; i <= n; i++) {
-            j = i + length - 1;
+        for (int i = 1; i <= n - length + 1; i++) {
+            int j = i + length - 1;
             subtree_exp[i][j] = INT_MAX;
-            subtree_cost[i][j] = subtree_cost[i][j - 1] + p[j] + q[j];
+            subtree_cost[i][j] = subtree_cost[i][j - 1] + p[j - 1] + q[j]; // TODO: check p[0..n-1]
             for (int r = i; r <= j; r++) {
-                int tmp = subtree_exp[i][r - 1] + subtree_exp[r + 1][j] + subtree_cost[i][j];
+                double tmp = subtree_exp[i][r - 1] + subtree_exp[r + 1][j] + subtree_cost[i][j];
                 if (tmp < subtree_exp[i][j]) {
                     subtree_exp[i][j] = tmp;
-                    root[i][j] = r;
+                    root[i][j] = r - 1;
                 }
             }
         }
     }
 
+    //TODO: clear
+    for (int i = 1; i <= n + 1; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            std::cout << subtree_cost[i][j] << "   ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    for (int i = 1; i <= n + 1; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            std::cout << subtree_exp[i][j] << "   ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    //=======
+
     return root;
 }
+
+//template<typename DataType>
+//BST<DataType> OptimalBST(std::vector<DataType *> &data, std::vector<double> &p, std::vector<double> &q) {
+//    assert((p.size() == data.size() == q.size() - 1) && "Invalid vectors");
+//
+//    auto rootMatrix = optimalBSTRootMatrix(p, q);
+//
+//
+//}
 
 #endif //OPTIMAL_BST_OPTIMAL_BST_HPP
