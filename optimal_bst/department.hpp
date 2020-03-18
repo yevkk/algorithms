839e2c3d113a2e21765cc07c305ec86f9c061fe9
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-03-18 12:33:40 +0200

    remove redundant constructor

--- optimal_bst/department.hpp
+++ optimal_bst/department.hpp
@@ -20,10 +20,6 @@ public:
             _group(group),
             _averagePoint(averagePoint) {};
 
-    Student(Student &student) :
-            _fullName(student.getFullName()),
-            _group(student.getGroup()),
-            _averagePoint(student.getAveragePoint()) {};
 
     std::string getFullName() {
         return _fullName;
