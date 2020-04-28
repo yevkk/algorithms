af8fbfab51cf3c69e3e0c46f0767214a65382473
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-04-28 17:11:58 +0300

    project initial commit

---
+++ fibonacci_heap/fibonacci_heap.hpp
@@ -0,0 +1,11 @@
+#ifndef FIBONACCI_HEAP_FIBONACCI_HEAP_HPP
+#define FIBONACCI_HEAP_FIBONACCI_HEAP_HPP
+
+template<typename DataType>
+class FibonacciHeap {
+
+};
+
+#include "fibonacci_heap.hxx"
+
+#endif //FIBONACCI_HEAP_FIBONACCI_HEAP_HPP
